1938c717a4c8b7423acc4c0405d434f353c55960
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-12-28 21:57:40 +0200

    Initial Commit

---
+++ Delegate&Protocol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Delegate&Protocol
+//
+//  Created by Ramill Ibragimov on 28.12.2019.
+//  Copyright © 2019 Ramill Ibragimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
